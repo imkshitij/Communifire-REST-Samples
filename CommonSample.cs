75ef5656e7738b2f984ecf5ab02de3c2d2b9cabd
raghav-khunger <raghav.khunger@gmail.com>
2013-04-23 17:25:29 +0530

    REST work for tags

--- CommonSample.cs
+++ CommonSample.cs
@@ -1,12 +1,10 @@
 ﻿#region Using Directives
 
 using System;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Net;
 using System.Text;
 using System.Xml;
-using System.Xml.Serialization;
 
 #endregion
 
@@ -14,6 +12,57 @@ namespace Communifire.RestApiSamples
 {
     public class CommonSample
     {
+        public static void GetSearchResults()
+        {
+            try
+            {
+                string serviceUrl = string.Format("{0}commonservice.svc/search?searchText={3}&startPage={1}&pageLength={2}", Program.ROOT_URL, 1, 30, "s");
+
+                //create a new HttpRequest
+                var myRequest = (HttpWebRequest)WebRequest.Create(serviceUrl);
+                myRequest.Method = "GET";
+
+                myRequest.Headers.Add("Rest-Api-Key", Program.API_KEY);
+
+
+                //post the request and get the response details
+                using (var response = myRequest.GetResponse())
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        //read the results string
+                        string result = reader.ReadToEnd();
+                        //check the results assuming XML is returned: note for JSON: use JSON stringfy
+                        XmlDocument resultsXml = new XmlDocument();
+                        resultsXml.LoadXml(result);
+                        bool isError = Convert.ToBoolean(resultsXml.GetElementsByTagName("IsError")[0].InnerText);
+                        if (isError)
+                        {
+                            string responseMessage = resultsXml.GetElementsByTagName("ResponseMessage")[0].InnerText;
+                            Console.Write(responseMessage);
+                        }
+                        else
+                        {
+                            string serviceResult = resultsXml.GetElementsByTagName("ResponseData")[0].InnerText;
+                            if (!string.IsNullOrEmpty(serviceResult))
+                            {
+                                Console.WriteLine("Method successfully called.");
+                                Console.WriteLine(Environment.NewLine + string.Format("Result: {0} ", serviceResult));
+                            }
+                            else
+                                Console.WriteLine("Method called but result is not accurate.");
+                        }
+                    }
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.Read();
+        }
+
         public static void GetGenericContent()
         {
             try
@@ -73,6 +122,203 @@ namespace Communifire.RestApiSamples
             }
             Console.Read();
         }
+
+        public static void GetTags()
+        {
+            try
+            {
+                string serviceUrl = string.Format("{0}commonservice.svc/tags?startPage={1}&pageLength={2}", Program.ROOT_URL, 1, 30);
+
+                //create a new HttpRequest
+                var myRequest = (HttpWebRequest)WebRequest.Create(serviceUrl);
+                myRequest.Method = "GET";
+
+                myRequest.Headers.Add("Rest-Api-Key", Program.API_KEY);
+
+
+                //post the request and get the response details
+                using (var response = myRequest.GetResponse())
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        //read the results string
+                        string result = reader.ReadToEnd();
+                        //check the results assuming XML is returned: note for JSON: use JSON stringfy
+                        XmlDocument resultsXml = new XmlDocument();
+                        resultsXml.LoadXml(result);
+                        bool isError = Convert.ToBoolean(resultsXml.GetElementsByTagName("IsError")[0].InnerText);
+                        if (isError)
+                        {
+                            string responseMessage = resultsXml.GetElementsByTagName("ResponseMessage")[0].InnerText;
+                            Console.Write(responseMessage);
+                        }
+                        else
+                        {
+                            string serviceResult = resultsXml.GetElementsByTagName("ResponseData")[0].InnerText;
+                            if (!string.IsNullOrEmpty(serviceResult))
+                            {
+                                Console.WriteLine("Method successfully called.");
+                                Console.WriteLine(Environment.NewLine + string.Format("Result: {0} ", serviceResult));
+                            }
+                            else
+                                Console.WriteLine("Method called but result is not accurate.");
+                        }
+                    }
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Adds the tags.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="entityID">The entity ID.</param>
+        /// <param name="tags">Tags in comma separated format.</param>
+        /// <param name="spaceID">The space ID.</param>
+        public static void AddTags(string tags, int spaceID, string entityType, int entityID)
+        {
+            try
+            {
+                string postData =
+    string.Format(
+        "<AddTags xmlns=\"http://tempuri.org/\"><tags>{0}</tags><spaceID>{1}</spaceID><entityType>{2}</entityType><entityID>{3}</entityID></AddTags>",
+      tags, spaceID, entityType, entityID);
+
+                string serviceUrl = string.Format("{4}commonservice.svc/tags?tags={0}&spaceID={1}&entityType={2}&entityID={3}", tags, spaceID, entityType, entityID, Program.ROOT_URL);
+
+                //create a new HttpRequest
+                var myRequest = (HttpWebRequest)WebRequest.Create(serviceUrl);
+                myRequest.Method = "POST";
+
+                //set the content header type. Note: use "application/json" for JSON
+                myRequest.ContentType = "application/xml";
+                byte[] data = Encoding.UTF8.GetBytes(postData);
+                myRequest.ContentLength = data.Length;
+
+                //add the API key
+                myRequest.Headers.Add("Rest-Api-Key", Program.API_KEY);
+
+                //add the data to be posted in the request stream
+                var requestStream = myRequest.GetRequestStream();
+                requestStream.Write(data, 0, data.Length);
+                requestStream.Close();
+
+
+
+                //post the request and get the response details
+                using (var response = myRequest.GetResponse())
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        //read the results string
+                        string result = reader.ReadToEnd();
+                        //check the results assuming XML is returned: note for JSON: use JSON stringfy
+                        XmlDocument resultsXml = new XmlDocument();
+                        resultsXml.LoadXml(result);
+                        bool isError = Convert.ToBoolean(resultsXml.GetElementsByTagName("IsError")[0].InnerText);
+                        if (isError)
+                        {
+                            string responseMessage = resultsXml.GetElementsByTagName("ResponseMessage")[0].InnerText;
+                            Console.Write(responseMessage);
+                        }
+                        else
+                        {
+                            string serviceResult = resultsXml.GetElementsByTagName("ResponseData")[0].InnerText;
+
+                            Console.WriteLine("Method successfully called.");
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.Read();
+        }
+
+        ///// <summary>
+        ///// Adds the tag.
+        ///// </summary>
+        ///// <param name="entityType">Type of the entity.</param>
+        ///// <param name="entityID">The entity ID.</param>
+        ///// <param name="tagName">Tag name</param>
+        ///// <param name="spaceID">The space ID.</param>
+        //public static void AddTag(string tagName,int spaceID ,int entityType,int entityID )
+        //{
+        //    try
+        //    {
+        //        string postData = string.Format("<TagDTO><EntityID>{0}</EntityID><EntityType>{1}</EntityType><SpaceID>{2}</SpaceID><TagName>{3}</TagName></TagDTO>",
+        //            entityID, entityType, spaceID, tagName);
+        //        //set the RESTful URL
+        //        string serviceUrl = string.Format("{0}commonservice.svc/tags/tag", Program.ROOT_URL);
+
+        //        //create a new HttpRequest
+        //        var myRequest = (HttpWebRequest)WebRequest.Create(serviceUrl);
+        //        myRequest.Method = "POST";
+        //        //set the content header type. Note: use "application/json" for JSON
+        //        myRequest.ContentType = "application/xml";
+        //        byte[] data = Encoding.UTF8.GetBytes(postData);
+        //        myRequest.ContentLength = data.Length;
+        //        //add the API key
+        //        myRequest.Headers.Add("Rest-Api-Key", Program.API_KEY);
+        //        //add the data to be posted in the request stream
+        //        var requestStream = myRequest.GetRequestStream();
+        //        requestStream.Write(data, 0, data.Length);
+        //        requestStream.Close();
+
+        //        //post the request and get the response details
+        //        using (var response = myRequest.GetResponse())
+        //        {
+        //            if (response.ContentLength > 0)
+        //            {
+        //                using (var reader = new StreamReader(response.GetResponseStream()))
+        //                {
+
+        //                    //read the results string
+        //                    string result = reader.ReadToEnd();
+        //                    //check the results assuming XML is returned: note for JSON: use JSON stringfy
+        //                    XmlDocument resultsXml = new XmlDocument();
+        //                    resultsXml.LoadXml(result);
+        //                    bool isError = Convert.ToBoolean(resultsXml.GetElementsByTagName("IsError")[0].InnerText);
+        //                    if (isError)
+        //                    {
+        //                        string responseMessage = resultsXml.GetElementsByTagName("ResponseMessage")[0].InnerText;
+        //                        Console.Write(responseMessage);
+        //                    }
+        //                    else
+        //                    {
+        //                        string serviceResult = resultsXml.GetElementsByTagName("ResponseData")[0].InnerText;
+        //                        if (!string.IsNullOrEmpty(serviceResult))
+        //                        {
+        //                            Console.WriteLine("Method successfully called.");
+        //                            Console.WriteLine(Environment.NewLine + string.Format("Result: {0} ", serviceResult));
+        //                        }
+        //                        else
+        //                            Console.WriteLine("Method called but result is not accurate.");
+        //                    }
+
+        //                }
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Failed because: 0 length content returned.");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        Console.WriteLine(exception.Message);
+        //    }
+        //}
 
     }// end class
 }// end namespace
